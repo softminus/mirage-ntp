1fb448cae6d3538f63f73789b0018bf6597cea09
Kia <2592730+superbaud@users.noreply.github.com>
2016-01-02 02:13:11 -0800

    i hope this works
    i'm in despair

--- lib/wire_structs.ml
+++ lib/wire_structs.ml
@@ -17,7 +17,7 @@ cstruct ntp {
     uint8_t     flags;
     uint8_t     stratum;
     int8_t      poll;
-    int8_t      precisions;
+    int8_t      precision;
     uint16_t    root_delay[2];
     uint16_t    root_dispersion[2];
     uint32_t    refid;
@@ -31,4 +31,20 @@ cstruct ntp {
     uint32_t    recv_ts[2];     (* T2: server-measured time when request arrives *)
     uint32_t    trans_ts[2];    (* T3: server-measured time when reply   departs *)
 } as big_endian
+
+
+type pkt = {
+    flags           : int;
+    stratum         : int;
+    poll            : int;
+    precision       : int;
+    root_delay      : int32;
+    root_dispersion : int32;
+    refid           : int32;
+
+    reference_ts    : ts;
+    origin_ts       : ts;
+    recv_ts         : ts;
+    trans_ts        : ts;
+}
 
