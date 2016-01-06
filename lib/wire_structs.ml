520a18813ca9c73be18926e76b08626c84780afb
Kia <2592730+superbaud@users.noreply.github.com>
2016-01-05 18:50:31 -0800

    why

--- lib/wire_structs.ml
+++ lib/wire_structs.ml
@@ -6,6 +6,11 @@ type ts = {
 let ts_to_int64 ts =
     Int64.add (Int64.of_int32 ts.fraction) (Int64.shift_left (Int64.of_int32 ts.seconds) 32)
 
+let int64_to_ts i =
+    let seconds =   Int64.to_int32 (Int64.shift_right_logical i 32) in
+    let fraction =  Int64.to_int32 i in
+    {seconds; fraction}
+
 
 type short_ts = {
     seconds: Cstruct.uint16; 
@@ -14,6 +19,11 @@ type short_ts = {
 
 let short_ts_to_int32 ts =
     Int32.add (Int32.of_int ts.fraction) (Int32.shift_left (Int32.of_int ts.seconds) 16)
+
+let int32_to_short_ts i =
+    let seconds =  Int32.to_int(Int32.shift_right_logical i 16) in
+    let fraction = Int32.to_int(Int32.logand (Int32.of_int 0xffff) i) in
+    {seconds; fraction}
 
 type date = {
     era: Cstruct.uint32;
@@ -68,4 +78,24 @@ let buf_of_pkt p =
     set_ntp_recv_ts         buf (ts_to_int64        p.recv_ts);
     set_ntp_trans_ts        buf (ts_to_int64        p.trans_ts);
 
+    buf
 
+
+
+
+(*
+let pkt_of_buf b =
+    if Cstruct.len b <> sizeof_ntp then
+        None
+    else
+        let flags           = get_ntp_flags     b in
+        let stratum         = get_ntp_stratum   b in
+        let poll            = get_ntp_poll      b in
+        let precision       = get_ntp_precision b in
+        let root_delay      = get_root_delay
+        let root_dispersion =
+        let refid           
+        let reference_ts    
+        let origin_ts       
+        let recv_ts         
+        let trans_ts        *)
