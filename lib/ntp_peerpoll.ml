475e07934d2f8d26e61b3948eb314a5151d690f8
Kia <2592730+superbaud@users.noreply.github.com>
2016-02-05 20:22:17 -0800

    got the wrong sense, sorry

--- lib/ntp_peerpoll.ml
+++ lib/ntp_peerpoll.ml
@@ -11,7 +11,15 @@ let validate_packet b ctx =
             if p.trans_ts   =   int64_to_ts Int64.zero  then None else (* server not sync'd *)
             if p.recv_ts    =   int64_to_ts Int64.zero  then None else (* server not sync'd *)
 
-            if p.trans_ts   <>  ctx.recv.fe_transmit    then None else (* we already saw this packet *)
+            if p.trans_ts   =   ctx.recv.fe_transmit    then None else (* we already saw this packet *)
             if p.origin_ts  <>  ctx.send.ne_transmit    then None else (* this packet doesn't have the timestamp
                                                                           we struck in it *)
             Some p
+
+
+
+
+
+let update_state_in p ctx =
+
+
