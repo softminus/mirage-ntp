41d5d5046c722745bb043d0f5947d27ef482df57
Kia <2592730+superbaud@users.noreply.github.com>
2016-03-10 18:32:00 -0800

    the NTP reference implementation and its constants are bad and should feel bad

--- lib/ntp_constants.ml
+++ lib/ntp_constants.ml
@@ -1,18 +1,2 @@
 (* here we store all the terrible constants *)
 
-
-(* phi, in units of seconds/seconds, represents the maximum frequency error that
- * we assume exists on our clock. If we multiply it by how long ago we've taken a
- * measurement, it gives us the maximum error of that measurement due to our clock's
- * frequency error, termed "dispersion"
- *)
-let phi = 15E-6
-
-
-(* rho, in seconds, represents the maximum error in each local clock measurement
- *
- * the error in a time measurement we took t seconds ago thus is
- * error(t) = rho + phi * t
- * 
- *)
-let rho = -18 (* -18 means just that rho = 2^-18 sec *)
