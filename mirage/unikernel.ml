110a51b7bcaa2dec35f900de3f042ce90e77f443
Kia <2592730+superbaud@users.noreply.github.com>
2016-05-26 04:57:23 -0700

    can send client packets

--- mirage/unikernel.ml
+++ mirage/unikernel.ml
@@ -9,26 +9,16 @@ let green fmt  = Printf.sprintf ("\027[32m"^^fmt^^"\027[m")
 let yellow fmt = Printf.sprintf ("\027[33m"^^fmt^^"\027[m")
 let blue fmt   = Printf.sprintf ("\027[36m"^^fmt^^"\027[m")
 
+let server = Ipaddr.V4.of_string_exn  "131.215.239.14"
+
 module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
     module U = S.UDPV4
 
   let start c s =
-    S.listen_udpv4 s ~port:123 (fun ~src ~dst ~src_port data ->
-        C.log_s c (green "new UDP frame from %s %d"
-                     (Ipaddr.V4.to_string src) src_port)
-        >>= fun () ->
-            let tsc_ts:ts = {seconds = Int32.of_int 3673250845; fraction = Int32.of_int 0} in
-            let udp = S.udpv4 s in
-            let pkt = pkt_of_buf data in
-            match pkt with
-        | None -> C.log_s c (yellow "fail")
-        | Some f -> U.write ~source_port:123 ~dest_ip:src ~dest_port:123 udp (buf_of_pkt(new_reply tsc_ts tsc_ts f tsc_ts))
-                
+      let udp = S.udpv4 s in
+      let no = Int64.of_int @@ Tsc.rdtsc() in
+      let txts:ts = int64_to_ts no in
+      U.write ~source_port:123 ~dest_ip:server ~dest_port:123 udp (buf_of_pkt (new_query txts));
 
-
-
-      );
-
-    S.listen s
 
 end
