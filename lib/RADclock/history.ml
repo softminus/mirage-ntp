638dea19c06f61c15e0f191f7c32ca14f1c737d0
Kia <2592730+superbaud@users.noreply.github.com>
2016-05-31 21:23:43 -0700

    min_and_where, complete version

--- lib/RADclock/history.ml
+++ lib/RADclock/history.ml
@@ -96,14 +96,25 @@ let resize h ncap =
     match h with History (cap, offset, l) ->
         History (ncap, offset, take ncap l)
 
+let rec min_where_aux extractor li =
+    match li with
+    | []    -> failwith "min_where_aux"
+    | [x]   -> (0, x)
+    | x::xs ->
+            let pz, z = min_where_aux extractor xs in
+            match (compare (extractor z) (extractor x) < 0) with
+            | true  -> (pz + 1, z)
+            | false -> (0,      x)
 
-let min_by extractor hist =
-    let cmp x y = if (compare (extractor x) (extractor y) ) > 0 then y else x
-    in
+let min_and_where extractor hist =
     match hist with History (cap, offset, l) ->
         match l with
-        | z::zs -> List.fold_left cmp z zs
         | [] -> failwith "min_by"
+        | z ->
+                let rv = min_where_aux extractor z in
+                let idx = fst rv in
+                let x   = snd rv in
+                (x, Fixed(idx, offset))
 
 let range_slice hist left right =
     let idx_l = idx_of_point hist left in
