d02f644e982b39b5b55531ccd559cc9eb14f71a2
Kia <2592730+superbaud@users.noreply.github.com>
2016-06-03 21:44:28 -0700

    tap.sh

---
+++ unikernel/tap.sh
@@ -0,0 +1,6 @@
+sudo ip tuntap add dev tap0 user $USER mode tap
+sudo ifconfig tap0 172.16.0.1  netmask 255.255.255.0
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+sudo iptables -t nat -A POSTROUTING -o wlp3s0 -j MASQUERADE
+sudo iptables -I FORWARD 1 -i tap0 -j ACCEPT
+sudo iptables -I FORWARD 1 -o tap0 -m state --state RELATED,ESTABLISHED -j ACCEPT
