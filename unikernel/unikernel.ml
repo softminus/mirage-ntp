91d272afed02c0f19bf2ae036465d80473eb376b
Kia <2592730+superbaud@users.noreply.github.com>
2016-06-03 19:17:37 -0700

    this doesn't quite work

--- unikernel/unikernel.ml
+++ unikernel/unikernel.ml
@@ -1,8 +1,8 @@
 open Lwt
 open Tsc
 open Wire
-open Ntp_client
 open Int64
+open Ntp_client
 
 let red fmt    = Printf.sprintf ("\027[31m"^^fmt^^"\027[m")
 let green fmt  = Printf.sprintf ("\027[32m"^^fmt^^"\027[m")
@@ -19,7 +19,8 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
         let udp = S.udpv4 stack in
         let st, push_st = Lwt_stream.create() in
         S.listen_udpv4 stack ~port:123 (fun ~src ~dst ~src_port data ->
-            push_st @@ Some data;
+            let ts = Int64.of_int @@ Tsc.rdtsc() in
+            push_st @@ Some (data, ts);
             Lwt.return ()
             );
             st
@@ -29,8 +30,8 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
             | None -> Lwt.fail (Failure "heck")
             | Some buf -> Lwt.return buf
 
-    let dump_packet c rxd txts =
-        let packet = validate_packet rxd txts in
+    let dump_packet c rxd nonce =
+        let packet = validate_reply rxd nonce in
         match packet with
             | None -> C.log_s c (yellow "fail")
             | Some pkt -> C.log_s c (Printf.sprintf "recv %Lx\ntrans %Lx\ntime %Lx" (ts_to_int64 pkt.recv_ts) (ts_to_int64 pkt.trans_ts)(Int64.of_int @@ Tsc.rdtsc()))
@@ -38,22 +39,41 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
             let st = connect_to_server s server in
             let udp = S.udpv4 s in
 
-            let nonce = Int64.of_int @@ Tsc.rdtsc() in
-            let txts:ts = int64_to_ts nonce in
-            C.log_s c (Printf.sprintf "send %Lx" (Int64.of_int @@ Tsc.rdtsc())) >>= fun () ->
+            let state = blank_state in
 
-            U.write ~source_port:123 ~dest_ip:server ~dest_port:123 udp (buf_of_pkt (new_query txts)) >>= fun () ->
+            let q = new_query in
+            C.log_s c (Printf.sprintf "send ONE %Lx" ((fst q).tsc)) >>= fun () ->
+            U.write ~source_port:123 ~dest_ip:server ~dest_port:123 udp (snd q) >>= fun () ->
             rx st >>= fun (rxd) ->
-            dump_packet c rxd txts >>= fun () ->
-            let nonce = Int64.of_int @@ Tsc.rdtsc() in
-            let txts:ts = int64_to_ts nonce in
-            C.log_s c (Printf.sprintf "send %Lx" (Int64.of_int @@ Tsc.rdtsc())) >>= fun () ->
-            U.write ~source_port:123 ~dest_ip:server ~dest_port:123 udp (buf_of_pkt (new_query txts)) >>= fun () ->
-            rx st >>= fun (rxd) ->
-            dump_packet c rxd txts >>= fun () ->
+            let state = add_sample state (fst rxd) (fst q) (snd rxd) in
+            Lwt.return(update_estimators state) >>= fun(state) ->
+            let x = state.estimators.rtt_hat in
+            C.log_s c (Printf.sprintf "rtt hat ONE %Lx" (History.point_of_history x)) >>= fun () ->
 
-            S.listen s
+
+
+                S.listen s 
 
 
 
 end
+
+
+(*
+
+
+
+            let nonce = Int64.of_int @@ Tsc.rdtsc() in
+            let txts:ts = int64_to_ts nonce in
+
+            dump_packet c rxd txts >>= fun () ->
+            let nonce = Int64.of_int @@ Tsc.rdtsc() in
+            let txts:ts = int64_to_ts nonce in
+            C.log_s c (Printf.sprintf "send %Lx" (Int64.of_int @@ Tsc.rdtsc())) >>= fun () ->
+            U.write ~source_port:123 ~dest_ip:server ~dest_port:123 udp (buf_of_pkt (new_query txts)) >>= fun () ->
+            rx st >>= fun (rxd) ->
+            dump_packet c rxd txts >>= fun () ->
+            S.listen s
+
+*)
+
