8e2f430978ddd4b03c1149651da21970584a4fd4
Kia <2592730+superbaud@users.noreply.github.com>
2016-06-03 20:52:56 -0700

    require ntp_client

--- unikernel/config.ml
+++ unikernel/config.ml
@@ -3,7 +3,7 @@ open Mirage
 let stack = generic_stackv4 default_console tap0
 
 let client =
-    let libraries = ["wire" ;"tsc" ] in
+    let libraries = ["wire" ;"tsc"; "ntp_client" ] in
   let packages = [  ] in
   foreign
     ~libraries ~packages
