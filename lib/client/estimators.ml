2f5aed98a4606e47db9ce10915ea380673ada5d1
Kia <2592730+superbaud@users.noreply.github.com>
2016-06-15 01:59:10 -0700

    return operators to estimators.ml
    return them so they don't cause conflict with LWT's >>= until we can
    figure out how to export them properly (something with mli files)

--- lib/client/estimators.ml
+++ lib/client/estimators.ml
@@ -3,6 +3,27 @@
 open Types
 open History
 
+let (>>=) x f =
+    match x with
+    | Some x    -> (f x)
+    | None      -> None
+
+let (<$>) f m =                 (* fmap *)
+    match m with
+     | Some x   -> Some (f x)
+     | None     -> None
+
+let (<*>) f m =
+    match f with
+    | Some ff   -> ff <$> m
+    | None      -> None
+
+let (<|>) l r =
+    match l with
+    | None      -> r
+    | x         -> x
+
+let join x = x >>= (fun x -> x)
 (* SHARED *)
 let rtt_of sample =
     let ts = (fst sample).timestamps in
