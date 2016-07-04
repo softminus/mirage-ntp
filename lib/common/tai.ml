947effd92fce8cb0493b3b140d6278c51adbc51b
Kia <2592730+superbaud@users.noreply.github.com>
2016-07-04 03:31:14 -0700

    tai.ml

--- lib/common/tai.ml
+++ lib/common/tai.ml
@@ -1,4 +1,4 @@
-(* 
+(*
  * NTP time is in the discontinuous UTC timescale and thus has duplicate values
  * because of leap seconds. This makes raw NTP timestamps unfit for feeding
  * into a rate/offset estimation algorithm.
@@ -11,7 +11,8 @@
  * discontinuity-free timescale from the NTP timestamps.
  *
  * (IEEE 1588 / PTP expresses timestamps in TAI and also includes the current
- * UTC/TAI offset in each packet. Similarly, GPS, BeiDou, and Galileo all use
+ * UTC/TAI offset in each packet and include a binary flag warning of an
+ * upcoming leap second event. Similarly, GPS, BeiDou, and Galileo all use
  * timescales with no leap seconds (effectively, TAI) and include the current
  * UTC offset (and the time of an upcoming leap second event) in the navigation
  * messages transmitted from the space vehicles. None of those systems ever
@@ -20,14 +21,42 @@
  * There is only a binary flag that indicates whether a leap second event is
  * coming -- the NTP client *must* either do complex calendar maths to find out
  * *when* the leap event is or (from an out-of-band source) have been provided
- * a copy of an up-to-date leap second table. It is difficult to come up with a
- * more pointlessly fragile, baroque, and overly complex system for coping with
- * leap second events. 
+ * a copy of an up-to-date leap second table.
  *
- * 
+ * It is difficult to come up with a more pointlessly fragile, baroque, and
+ * overly complex system for coping with leap second events.
+ *
  *
  * Unfortunately, we are required to implement this logic to have a correct NTP
  * client.
+ *
+ *
+ * We check to see if we have a valid and up-to-date leap table -- if so, we
+ * know the current TAI offset, the time of the next leap second event, and the
+ * TAI offset after the leap event. We can thus use it to unbake UTC timestamps
+ * into TAI timestamps that can be used for rate/offset estimation (and bake in
+ * the TAI offset as desired for consumers who want UTC time).
+ *
+ * If we do not have an up-to-date leap table we:
+     * don't know the current TAI offset
+     * we don't know the time of the leap second event
+ *
+ * The only information that NTP packets contain about leap second events is a
+ * binary flag that indicates an upcoming leap second at the next UTC midnight.
+ * This means we need to manually calculate when the next UTC midnight is, and
+ * proceed accordingly.
+ *
+ * We also need to verify that the upcoming UTC midnight is the end of 30 June
+ * or 31 December because some NTP servers set the leap flag prematurely -- see
+ * doi://10.1007/978-3-319-30505-9_29. This is where the requirement for
+ * calendar arithmetic comes in.
+ *
+ * All of this would be immaterial if NTP unambiguously encoded the current
+ * TAI time and the current TAI/UTC offset, along with the TAI time of any
+ * upcoming leap second event and the TAI/UTC offset after leap second event
+ * -- like GPS. Most NTP servers get time information from GPS so this would
+ * not even be a difficult change.
+ *
  *)
 
 type tai = Int64
@@ -42,5 +71,4 @@ type leap_table = {
     end_of_validity:    utc;
     events:             leap_event list;
     }
-    
 let tai_to_utc = 0
