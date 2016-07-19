97e3616761e5617b95128d6cdaef5de44a8ec2e7
Kia <2592730+superbaud@users.noreply.github.com>
2016-07-18 22:30:31 -0700

    absolute_time doesnt return a Maybe

--- lib/clocklib/diffabs.ml
+++ lib/clocklib/diffabs.ml
@@ -7,7 +7,7 @@ let absolute_time alg_out tsc =
         | None              -> 0.0
     in
     match (alg_out.freshness, alg_out.p_hat_and_error, alg_out.ca_and_error) with
-    | (last_tsc, (p_hat, _), (ca, _))   -> Some ((Int64.to_float tsc) *. p_hat +. ca +. (Int64.to_float (delta_TSC tsc last_tsc)) *. fixup_local p_hat alg_out)
+    | (last_tsc, (p_hat, _), (ca, _))   -> ((Int64.to_float tsc) *. p_hat +. ca +. (Int64.to_float (delta_TSC tsc last_tsc)) *. fixup_local p_hat alg_out)
 
 
 let inSKM   alg_out tsc =
