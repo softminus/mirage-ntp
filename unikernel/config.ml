5062c074b16ed6f375c75ca640240bf6656478dc
Kia <2592730+superbaud@users.noreply.github.com>
2016-07-25 21:08:00 -0700

    nocrypto in unikernel

--- unikernel/config.ml
+++ unikernel/config.ml
@@ -1,15 +1,15 @@
 open Mirage
 
-let stack = generic_stackv4 default_console tap0
+let net = generic_stackv4 default_console tap0
 
 let client =
     let libraries = ["wire" ;"tsc"; "ntp_client";"clocklib" ] in
-  let packages = [  ] in
+    let packages = [  ] in
   foreign
-    ~libraries ~packages
-    "Unikernel.Main" @@ console @-> stackv4 @-> job
+    ~libraries ~packages ~deps:[abstract nocrypto]
+    "Unikernel.Main" (console @-> stackv4 @-> job)
 
 let () =
     register "network" [
-        client $ default_console $ stack
+        client $ default_console $ net
   ]
--- unikernel/unikernel.ml
+++ unikernel/unikernel.ml
@@ -6,7 +6,7 @@ open Int64
 open Ntp_client
 open OS
 open Diffabs
-
+open Nocrypto
 
 let server = Ipaddr.V4.of_string_exn  "131.215.239.14"
 
@@ -42,14 +42,14 @@ module Main (C: V1_LWT.CONSOLE) (S: V1_LWT.STACKV4) = struct
         match packet with
             | None -> C.log_s c ("fail")
             | Some pkt -> C.log_s c (Printf.sprintf "recv %Lx\ntrans %Lx\ntime %Lx" (ts_to_int64 pkt.recv_ts) (ts_to_int64 pkt.trans_ts)(Int64.of_int @@ Tsc.rdtsc()))
-        let start c s =
+        let start c s () =
             let st = connect_to_server s server in
             let udp = S.udpv4 s in
 
             let rec do_it state = function
                 | 0 -> Lwt.return (state)
-                | n -> (let q = new_query (Int64.of_int @@ Tsc.rdtsc()) in
-                        C.log_s c (Printf.sprintf "send ONE %Lx" ((fst q).tsc)) >>= fun () ->
+                | n -> (let q = new_query (Int64.of_int @@ Tsc.rdtsc()) (Nocrypto.Rng.Int64.gen_bits 64) in
+                        C.log_s c (Printf.sprintf "send ONE %Lx" ((fst q).when_sent)) >>= fun () ->
                         U.write ~source_port:123 ~dest_ip:server ~dest_port:123 udp (snd q) >>= fun () ->
                         rxto st >>= fun (wrapped) ->
                             match wrapped with
